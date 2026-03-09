0252aeac35121e1006634c6da2121ef7d0fb2a5e
edwinnelly <112112833+edwinnelly@users.noreply.github.com>
2026-03-09 13:55:28 +0100

    done

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,9 +1,7 @@
-// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // generates static files in `out`
+  output: "standalone"
 };
 
 export default nextConfig;
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
