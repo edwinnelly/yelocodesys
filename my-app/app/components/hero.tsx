7425c1c15a6235bd50bf8a46615adbf0a97c4c62
edwinnelly <112112833+edwinnelly@users.noreply.github.com>
2026-03-18 13:56:51 +0100

    done

--- my-app/app/components/hero.tsx
+++ my-app/app/components/hero.tsx
@@ -30,7 +30,8 @@ export default function Hero() {
         >
           {/* https://static.vecteezy.com/system/resources/previews/013/279/729/mp4/young-girl-which-applying-virtual-reality-headset-during-working-at-home-in-the-evening-modern-technology-concept-free-video.mp4 */}
           <source
-            src="/pics/abc.mp4"
+            src="https://static.vecteezy.com/system/resources/previews/013/279/729/mp4/young-girl-which-applying-virtual-reality-headset-during-working-at-home-in-the-evening-modern-technology-concept-free-video.mp4"
+            // src="/pics/abc.mp4"
             type="video/mp4"
           />
           {/* Fallback for browsers that don't support video */}
